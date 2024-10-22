0aad5624444f3108928f5599fc658c5a1f4e1d9e
leonard <vp3ebj7j@duck.com>
2024-10-22 14:36:56 +0200

    bidirectional prop

--- src/main/java/codeql/BidirectionalPropagation.java
+++ src/main/java/codeql/BidirectionalPropagation.java
@@ -3,21 +3,18 @@ package codeql;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class BidirectionalPropagation {
 
-    private static final Logger logger = LoggerFactory.getLogger(SensitiveDataInMap_OnlyKeyAsIndicator.class);
+    private static final Logger logger = LoggerFactory.getLogger(BidirectionalPropagation.class);
+
     public static void main(String[] args) {
         String s = "Pass@0rd";
-        Map<String, String> json = new HashMap<>();
-        String key = "password";
-        json.put(key, s);
-
-        String res = json.get(key);
-        String password = res;
-        String noname = password;
-        logger.debug("Result from map: " + noname);
+        logger.debug("Contents of s: " + s);
+        String password = s;
+        doWork(password);
     }
+
+    private static void doWork(String password) {
+    }
+
 }
